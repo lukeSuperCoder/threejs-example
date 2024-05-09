9f1870b3bb022fd8ee873749843233107850f2fd
lukeSuperCoder <lc970820@163.com>
2024-05-09 14:27:41 +0800

    新增环境贴图

--- add_gltf.js
+++ add_gltf.js
@@ -18,6 +18,9 @@ function init() {
 
 	scene = new THREE.Scene();
 
+	const light = new THREE.PointLight( 0xffffff, 10 );
+	scene.add( light );
+
 	new RGBELoader()
 		.setPath( './assets/model/textures/equirectangular/' )
 		.load( 'royal_esplanade_1k.hdr', function ( texture ) {		//配置环境相关
@@ -40,7 +43,9 @@ function init() {
 				// .load()方法加载图像，返回一个纹理对象Texture
 				const texture_water = texLoader.load('./assets/model/textures/water.jpg');
 				// mesh.material.map = texture_water;
-
+				const textureCube = new THREE.CubeTextureLoader()
+				.load(['./assets/model/textures/water.jpg','./assets/model/textures/water.jpg','./assets/model/textures/water.jpg','./assets/model/textures/water.jpg','./assets/model/textures/water.jpg','./assets/model/textures/water.jpg','./assets/model/textures/water.jpg']);
+				
 				// 递归遍历所有模型节点批量修改材质
 				gltf.scene.traverse(function(obj) {
 					if (obj.isMesh) {//判断是否是网格模型
@@ -48,10 +53,19 @@ function init() {
 						console.log('模型节点名字',obj.name);
 						// 重新设置材质
 						// obj.material = new THREE.MeshStandardMaterial({
-						// 	color:0xff0000,
+						// 	// color:0xff0000,
+						// 	metalness: 0.3,//表示材质像金属的程度, 非金属材料,如木材或石材,使用0.0,金属使用1.0。
+						// 	roughness: 0.1,//表面粗糙度0.0表示平滑的镜面反射,1.0表示完全漫反射,默认0.5。
+						// 	envMapIntensity: 0.5,	//属性主要用来设置模型表面反射周围环境贴图的能力
+						// 	envMap:textureCube,
+						// 	clearcoat: 1,//物体表面清漆层或者说透明涂层的厚度
+						// 	clearcoatRoughness: 0.3,//透明涂层表面的粗糙度
+						// 	ior: 2.2,//折射率
+						// 	transmission: 1, //玻璃材质透光率，transmission替代opacity
 						// });
 					}
 				});
+				scene.environment = textureCube;
 				//用代码方式解决mesh共享材质问题
 				// gltf.scene.getObjectByName("小区房子").traverse(function (obj) {
 				// 	if (obj.isMesh) {
